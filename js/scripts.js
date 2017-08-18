06f77b2cb5e2369408a94da0f03711b8d865e49c
BartPu <bpuchalski@tlen.pl>
2017-08-18 11:33:15 +0200

    commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function drawTree(row) {
+    for (var i = 0; i < row; i++) {
+        var star = "";
+        for (var j = 0; j <= i; j++) star += '*';
+        console.log(star);
+
+    }
+
+}
+drawTree(5);
